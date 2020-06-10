117fe2e0a7fc2f7cbe1e04de5230b979227c4c53
vmaravind96 <aravind.kumar315@gmail.com>
2020-06-10 10:50:45 +0530

    Assignment 5 done

---
+++ Assignment5/assignment.js
@@ -0,0 +1,14 @@
+const task1 = document.getElementById("task-1");
+task1.style.color = "white";
+
+const task12 = document.querySelector("#task-1");
+task12.style.backgroundColor = "black";
+
+const title = document.querySelector("title");
+title.textContent = "Assignment Solved!";
+
+const title1 = document.head.querySelector("title");
+console.dir(title1);
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Assignment Solved";
